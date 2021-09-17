3c29b0b9cb6ba82791930179d57a281322f0e7ae
mehmeow <syarezan.im@gmail.com>
2021-09-18 01:58:11 +0800

    outbreak crawled

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
+const HtmlTableToJson = require('html-table-to-json');
 const got = require('got');
 const jsdom = require("jsdom");
+const { text } = require('cheerio/lib/api/manipulation');
 const { JSDOM } = jsdom;
 
 /* GET home page. */
@@ -10,15 +11,28 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// get content got
+// get outbreak stats
 router.get('/getstats', function (req, res, next) {
   got('https://www.outbreak.my/stats').then(response => {
     const dom = new JSDOM(response.body);
-    console.log(dom.window.document.querySelector('table.table').textContent);
+    const table = dom.window.document.querySelector('#casesTable').outerHTML;
+    // convert table to json
+    const jsonTables = HtmlTableToJson.parse(table);
+    res.status(200).send(jsonTables.results[0]);
   }).catch(err => {
     console.log(err);
   });
-  res.status(200).send({'cheerio':true});
+});
+
+// get outbreak map
+router.get('/getmap', function (req, res, next) {
+  got('https://www.outbreak.my/api/cases/map').then(response => {
+    // clean html
+    const clean = response.body.replace(/(<([^>]+)>)/gi, "");
+    res.status(200).send(JSON.parse(clean).data);
+  }).catch(err => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
