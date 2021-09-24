d253a184c16c7d66a2286a99e1aea0079191ad53
mehmeow <syarezan.im@gmail.com>
2021-09-24 14:12:48 +0800

    live api

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const got = require('got');
 const jsdom = require("jsdom");
 const { text } = require('cheerio/lib/api/manipulation');
 const { JSDOM } = jsdom;
+require('dotenv').config();
+const axios = require('axios').default;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -33,6 +35,33 @@ router.get('/getmap', function (req, res, next) {
   }).catch(err => {
     console.log(err);
   });
+});
+
+// get dummy detailed recipes
+router.post('/random', function (req, res, next) {
+  // const data = require("./recipes.json");
+  const filters = req.body;
+  const params = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    ...filters
+  }
+
+  axios.get(process.env.REACT_APP_API + "/random", { params })
+    .then(response => {
+      res.status(200).send({ data: response.data, count: response.data.recipes.length });
+    });
+});
+
+// get dummy nutrition
+router.get('/nutrition', function (req, res, next) {
+  const data = require("./nutrition.json");
+  res.status(200).send(data);
+});
+
+// get dummy nutrition
+router.get('/taste', function (req, res, next) {
+  const data = require("./taste.json");
+  res.status(200).send(data);
 });
 
 module.exports = router;
